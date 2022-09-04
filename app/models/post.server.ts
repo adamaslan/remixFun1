ed56255046c5cd0727fa9b80ba8abd6ab5b6a7db
Adam Aslan <chillcoders@gmail.com>
2022-09-04 19:57:41 -0400

    acts

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -9,3 +9,7 @@ export async function getPosts() {
 export async function getPost(slug: string) {
   return prisma.post.findUnique({ where: { slug } });
 }
+
+export async function createPost(post: any) {
+  return prisma.post.create({ data: post });
+}
--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -1,3 +1,62 @@
+import { redirect } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+
+import { createPost } from "~/models/post.server";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+
+  const title = formData.get("title");
+  const slug = formData.get("slug");
+  const markdown = formData.get("markdown");
+
+  await createPost({ title, slug, markdown });
+
+  return redirect("/posts/admin");
+};
+
+
 export default function NewPost() {
-    return <h2>New Post</h2>;
-  }
+  return (
+    <Form method="post">
+      <p>
+        <label>
+          Post Title:{" "}
+          <input
+            type="text"
+            name="title"
+            className={inputClassName}
+          />
+        </label>
+      </p>
+      <p>
+        <label>
+          Post Slug:{" "}
+          <input
+            type="text"
+            name="slug"
+            className={inputClassName}
+          />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="markdown">Markdown:</label>
+        <br />
+        <textarea
+          id="markdown"
+          rows={20}
+          name="markdown"
+          className={`${inputClassName} font-mono`}
+        />
+      </p>
+      <p className="text-right">
+        <button
+          type="submit"
+          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+        >
+          Create Post
+        </button>
+      </p>
+    </Form>
+  );
+}
